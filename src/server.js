7016baf71cbc2636d187c11ac4dab428e1b8d289
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2023-04-19 20:55:44 -0300

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,7 +1,48 @@
     import http from 'node:http'
 
+    // - HTTP
+    //      - Método HTTP
+    //      -- URL
+
+    // GET, POST, PUT, PATCH, DELETE
+
+    // GET => Buscar um recurso  do back-end
+    // POST => Criar um recurso no back-end
+    // PUT => Atualizar um recurso no back-end
+    // PATCH => Atualizar uma informação específica de um recurso do back-end
+    // DELETE => Deletar um recurso do back-end
+
+    // GET /users => Buscando usuários no back-end
+    // POST /users => Criar um usuário no back-end
+
+    // Stateful - Stateless
+
+    // Cabeçalhos (Requisição/resposta) => MEtadados
+
+    // HTTP Status Code
+
+    const users = []
+
     const server = http.createServer((req, res) => {
-        return res.end('Hello World')
+        const { method, url } = req
+
+        if (method == 'GET' && url == '/users') {
+            return res
+                .setHeader('Content-type', 'application/json')
+                .end(JSON.stringify(users))
+        } 
+
+        if (method == 'POST' && url == '/users') {
+            users.push({
+                id: 1,
+                name: 'Fulano',
+                email: 'fulano@exemplo.com',
+            })
+
+            return res.writeHead(201).end()
+        }
+
+        return res.writeHead(404).end()
     })
 
     server.listen(3333)
