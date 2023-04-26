c9578be087e0e6d5d47fed1b8d1cdcf9765c97af
Clelton Mielke <60109916+cleltonjm@users.noreply.github.com>
2023-04-26 20:06:23 -0300

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,48 +1,52 @@
-    import http from 'node:http'
+import http from 'node:http'
+import { json } from './middlewares/json.js'
 
-    // - HTTP
-    //      - Método HTTP
-    //      -- URL
+// - HTTP
+//      - Método HTTP
+//      -- URL
 
-    // GET, POST, PUT, PATCH, DELETE
+// GET, POST, PUT, PATCH, DELETE
 
-    // GET => Buscar um recurso  do back-end
-    // POST => Criar um recurso no back-end
-    // PUT => Atualizar um recurso no back-end
-    // PATCH => Atualizar uma informação específica de um recurso do back-end
-    // DELETE => Deletar um recurso do back-end
+// GET => Buscar um recurso  do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso do back-end
+// DELETE => Deletar um recurso do back-end
 
-    // GET /users => Buscando usuários no back-end
-    // POST /users => Criar um usuário no back-end
+// GET /users => Buscando usuários no back-end
+// POST /users => Criar um usuário no back-end
 
-    // Stateful - Stateless
+// Stateful - Stateless
 
-    // Cabeçalhos (Requisição/resposta) => MEtadados
+// Cabeçalhos (Requisição/resposta) => MEtadados
 
-    // HTTP Status Code
+// HTTP Status Code
 
-    const users = []
+const users = []
 
-    const server = http.createServer((req, res) => {
-        const { method, url } = req
+const server = http.createServer(async (req, res) => {
+    const { method, url } = req
 
-        if (method == 'GET' && url == '/users') {
-            return res
-                .setHeader('Content-type', 'application/json')
-                .end(JSON.stringify(users))
-        } 
+    await json(req, res)
 
-        if (method == 'POST' && url == '/users') {
-            users.push({
-                id: 1,
-                name: 'Fulano',
-                email: 'fulano@exemplo.com',
-            })
+    if (method == 'GET' && url == '/users') {
+        return res
+            .end(JSON.stringify(users))
+    }
 
-            return res.writeHead(201).end()
-        }
+    if (method == 'POST' && url == '/users') {
+        const { name, email } = req.body
 
-        return res.writeHead(404).end()
-    })
+        users.push({
+            id: 1,
+            name,
+            email,
+        })
 
-    server.listen(3333)
+        return res.writeHead(201).end()
+    }
+
+    return res.writeHead(404).end()
+})
+
+server.listen(3333)
